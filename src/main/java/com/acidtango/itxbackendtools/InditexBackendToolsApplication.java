38d4fcf783b5f374df9d44127a98758938e622d5
Daute Rodríguez <daute@acidtango.com>
2023-09-04 07:37:45 +0100

    initial commit

---
+++ src/main/java/com/acidtango/itxbackendtools/InditexBackendToolsApplication.java
@@ -0,0 +1,13 @@
+package com.acidtango.itxbackendtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexBackendToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InditexBackendToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acidtango/itxbackendtools/InditexBackendToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acidtango.itxbackendtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InditexBackendToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
