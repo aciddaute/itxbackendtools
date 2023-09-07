9d0445b40d65140ce640b670e5e432c7dd5347a1
Daute Rodríguez <daute@acidtango.com>
2023-09-07 09:08:05 +0100

    fix: product stock primitives

--- src/main/java/com/acidtango/itxbackendtools/catalog/domain/ProductStock.java
+++ src/main/java/com/acidtango/itxbackendtools/catalog/domain/ProductStock.java
@@ -4,6 +4,8 @@ import com.acidtango.itxbackendtools.shared.domain.ValueObject;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ProductStock extends ValueObject {
 
@@ -15,23 +17,28 @@ public class ProductStock extends ValueObject {
 
     static ProductStock zero() {
         HashMap<ProductSize, StockAmount> stock = new HashMap<>();
-        Arrays.asList(ProductSize.values())
-                .forEach(size -> stock.put(size, StockAmount.zero()));
+        Arrays.asList(ProductSize.values()).forEach(size -> stock.put(size, StockAmount.zero()));
 
         return new ProductStock(stock);
     }
 
-    static ProductStock fromPrimitives(HashMap<ProductSize, StockAmount> primitives) {
-        return new ProductStock(primitives);
+    static ProductStock fromPrimitives(HashMap<ProductSize, Integer> primitives) {
+
+        HashMap<ProductSize, StockAmount> stock =
+                primitives.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey,
+                        entry -> StockAmount.fromPrimitives(entry.getValue()),
+                        (firstEntry, secondEntry) -> firstEntry, HashMap::new));
+
+        return new ProductStock(stock);
     }
 
-    HashMap<ProductSize, StockAmount> toPrimitives() {
-        return stock;
+    HashMap<ProductSize, Integer> toPrimitives() {
+        return stock.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey,
+                entry -> entry.getValue().getValue(), (firstEntry, secondEntry) -> firstEntry, HashMap::new));
     }
 
     public boolean hasTotalStock(Integer expectedTotalStock) {
         Integer totalStock = stock.values().stream().map(StockAmount::getValue).reduce(Integer::sum).orElse(0);
-        
         return totalStock.equals(expectedTotalStock);
     }
 }
--- src/main/java/com/acidtango/itxbackendtools/catalog/domain/primitives/ProductPrimitives.java
+++ src/main/java/com/acidtango/itxbackendtools/catalog/domain/primitives/ProductPrimitives.java
@@ -1,9 +1,8 @@
 package com.acidtango.itxbackendtools.catalog.domain.primitives;
 
 import com.acidtango.itxbackendtools.catalog.domain.ProductSize;
-import com.acidtango.itxbackendtools.catalog.domain.StockAmount;
 
 import java.util.HashMap;
 
-public record ProductPrimitives(Integer id, String name, HashMap<ProductSize, StockAmount> stock) {
+public record ProductPrimitives(Integer id, String name, HashMap<ProductSize, Integer> stock) {
 }
