c6fa33605a58066695f9370b4c7106141c5b1e34
Daute Rodríguez <daute@acidtango.com>
2023-10-10 06:51:14 +0100

    fix: context loading test

--- src/test/java/com/acidtango/itxbackendtools/InditexBackendToolsApplicationTests.java
+++ src/test/java/com/acidtango/itxbackendtools/InditexBackendToolsApplicationTests.java
@@ -1,13 +1,17 @@
 package com.acidtango.itxbackendtools;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@AutoConfigureMockMvc
 @SpringBootTest
+@ActiveProfiles("mongo")
 class InditexBackendToolsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void context_loads() {
+    }
 
 }
